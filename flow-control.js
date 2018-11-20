6008bf03f8dd40a82f7354654dffe61943bc92bd
Stephanie Johnson <stephaniejohnson0402@gmail.com>
2018-11-20 09:36:42 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,19 @@
-function basicTeenager(age) { === 13-19
+function basicTeenager(13-19) { return "You are a teenager"
+
 }
 
-function teenager(13-19) {
+function teenager(age) {
+
 }
 
-function ageChecker(13-19) {
+function ageChecker(age) {
+
 }
 
 function ternaryTeenager(age) {
+
 }
 
 function switchAge(age) {
+
 }
